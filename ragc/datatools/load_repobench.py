976f66ec174a99dd87131dc9e06e376e710832f4
KonstFed <konstantin_fedorov_03@mail.ru>
2025-03-04 18:01:56 +0300

    repobench load fix

--- ragc/datatools/load_repobench.py
+++ ragc/datatools/load_repobench.py
@@ -1,4 +1,3 @@
-import json
 import shutil
 import subprocess
 import argparse
@@ -8,12 +7,12 @@ from datetime import datetime
 
 import pandas as pd
 from tqdm import tqdm
-from pathlib import Path
 from datasets import load_dataset
 
 CUR_FILE_P = Path(__file__).parent.resolve()
 LOAD_SCRIPT_PATH = CUR_FILE_P / "load_repo.bash"
-N_PROCESSES = 8
+N_PROCESSES = 4
+
 
 def load_repo(data: tuple[str, Path, datetime]) -> tuple[str, bool, str, str, str]:
     repo_name, save_path = data
@@ -23,17 +22,26 @@ def load_repo(data: tuple[str, Path, datetime]) -> tuple[str, bool, str, str, st
     if save_path.exists():
         return repo_name, False, "", "", f"Already exists {save_path}"
 
-    result = subprocess.run(["bash", LOAD_SCRIPT_PATH, repo_name, save_path], capture_output=True, text=True)
+    result = subprocess.run(
+        ["bash", LOAD_SCRIPT_PATH, repo_name, save_path], capture_output=True, text=True
+    )
     commit_hash = result.stdout.strip().split("\n")[-1]
     return repo_name, result.returncode == 0, commit_hash, result.stdout, result.stderr
 
+
 def multiprocess_load(repos2load: set[str], save_path: Path) -> pd.DataFrame:
     with Pool(processes=N_PROCESSES) as pool:
-        results = tqdm(pool.imap(load_repo, [(r, save_path) for r in repos2load]), total=len(repos2load))
-        results = pd.DataFrame(results, columns = ["repo", "status", "commit_hash", "stdout", "stder"])
+        results = tqdm(
+            pool.imap_unordered(load_repo, [(r, save_path) for r in repos2load]),
+            total=len(repos2load),
+        )
+        results = pd.DataFrame(
+            results, columns=["repo", "status", "commit_hash", "stdout", "stder"]
+        )
         results["reponame"] = results["repo"].apply(lambda p: p.split("/")[-1])
 
     return results
+
 
 def load(save_path: Path, clean: bool = False) -> None:
     dataset = load_dataset("tianyang/repobench_python_v1.1")
@@ -47,9 +55,9 @@ def load(save_path: Path, clean: bool = False) -> None:
         repos2load = repos2load.difference(set(meta["repo"]))
 
     current_n_repo = len(repos2load)
-    
+
     results = multiprocess_load(repos2load, save_path)
-    loaded_results = results.loc[results["status"],["repo", "reponame", "commit_hash"]]
+    loaded_results = results.loc[results["status"], ["repo", "reponame", "commit_hash"]]
 
     print(f"Loaded {len(loaded_results)}/{current_n_repo}")
 
@@ -67,7 +75,9 @@ def load(save_path: Path, clean: bool = False) -> None:
         return
 
     verified_folders = set(meta["reponame"])
-    folder_iter = lambda: filter(lambda p: p.is_dir() and not p.stem.startswith("."), save_path.iterdir())
+    folder_iter = lambda: filter(
+        lambda p: p.is_dir() and not p.stem.startswith("."), save_path.iterdir()
+    )
     for folder in tqdm(list(folder_iter())):
         if folder.name in verified_folders:
             continue
@@ -79,17 +89,15 @@ def load(save_path: Path, clean: bool = False) -> None:
             meta.loc[meta["reponame"] == folder.name, "check"] = True
 
     failed_repos = meta[~meta["check"]]
-    assert len(failed_repos) == 0, f"Sanity check failed, some verified files are incorrect in meta.csv for {len(failed_repos)} repos."
-
+    assert len(failed_repos) == 0, (
+        f"Sanity check failed, some verified files are incorrect in meta.csv for {len(failed_repos)} repos."
+    )
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='load repobench with repositories')
-    parser.add_argument('output', type=Path)
-    parser.add_argument('-c', '--clean', action="store_true")
+    parser = argparse.ArgumentParser(description="load repobench with repositories")
+    parser.add_argument("output", type=Path)
+    parser.add_argument("-c", "--clean", action="store_true")
 
     args = parser.parse_args()
     load(args.output, args.clean)
-
-    
-
