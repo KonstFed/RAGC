6391c8460144a9baa9e12b3a3d17e5594e40f824
KonstFed <konstantin_fedorov_03@mail.ru>
2025-03-04 14:42:18 +0000

    basic inference

--- ragc/inference.py
+++ ragc/inference.py
@@ -1,14 +1,48 @@
 from pathlib import Path
 
-from ragc.llms import OllamaWrapper
-from ragc.retrieval.pagerank import PageRankRetrieval
+import ollama
+
+from ragc.retrieval.node_retrieval import FileEmbRetrieval, LowGranularityRetrieval
 from ragc.graphs import SemanticParser
 
-if __name__ == "__main__":
-    repo_path = Path("/home/konstfed/Documents/diplom/RAGC/data/repositories/holostyak-bot")
+
+
+def generate_prompt(query, relevant_snippets):
+    prompt = "You are an expert programming assistant. Your task is to help me with the following snippets that may help you:\n\n"
+
+    for i, (file_id, code) in enumerate(relevant_snippets, start=1):
+        c_snippet = f"{i}. **Code Snippet {i}** from **{file_id}**:\n```\n{code}\n```\n"
+        prompt += c_snippet + "\n"
+
+    prompt += f"**Question/Task**: {query}\n\n"
+    prompt += """Provide the following in your response:
+- A clear explanation of your solution or answer.
+- The modified or generated code (if applicable).
+- Any additional notes or best practices related to the task.
+
+Let’s get started!"""
+
+    return prompt
+
+
+def inference(repo_path: Path, query: str) -> str:
     parser = SemanticParser()
-    graph = parser.parse_into_files(repo_path)
-    retrieval = PageRankRetrieval(repo_path=repo_path, semantic_graph=graph)
-    relevant = retrieval.retrieve("", 5)
-    for file, el in relevant:
-        print(file)
+    retrieval = LowGranularityRetrieval(repo_path, parser, "unclemusclez/jina-embeddings-v2-base-code")
+
+    relevant_snippets = retrieval.retrieve(query, 5)
+    prompt = generate_prompt(query, relevant_snippets)
+    
+    stream = ollama.chat(model='deepseek-r1', messages=[
+      {
+        'role': 'user',
+        'content': prompt,
+      },
+    ],     stream=True,
+)
+    for chunk in stream:
+      print(chunk['message']['content'], end='', flush=True)
+
+if __name__ == "__main__":
+    repo_path = Path("/home/jovyan/thesis/diplom/RAGC/holostyak-bot")
+    query = "Create DB for admin users"
+    print(inference(repo_path, query))
